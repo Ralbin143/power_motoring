ce86aa524b7c1495996ead647715805f7d76b766
Ralbin <ralbin@ahydratech.com>
2024-05-17 09:01:18 +0530

    Update api_const.dart

--- flutter_app/lib/data/api/api_const.dart
+++ flutter_app/lib/data/api/api_const.dart
@@ -1,9 +1,9 @@
-const String appUrl = "192.168.29.227:6008";
-// const String appUrl = "api.powermotoring.com";
+// const String appUrl = "192.168.29.227:6008";
+const String appUrl = "api-new.powermotoring.com";
 
 // Subscription
 const String checkSubscriptionUrl =
-    'http://$appUrl/api/user/check-subscription-status';
+    'https://$appUrl/api/user/check-subscription-status';
 
 const String enablePreLaunchOfferUrl =
-    'http://$appUrl/api/user/check-subscription-status';
+    'https://$appUrl/api/user/check-subscription-status';
