23be507c81da625006deceb4ef16e3aa73d8bde2
PowerMotoring <powermotoringapp@gmail.com>
2024-05-21 09:13:50 +0530

    21 morning
    21st morning

--- flutter_app/lib/const/urls.dart
+++ flutter_app/lib/const/urls.dart
@@ -1,5 +1,11 @@
+<<<<<<< Updated upstream
 // const String apiURl = "api.powermotoring.com";
 const String apiURl = "api-new.powermotoring.com";
 // const String apiURl = "192.168.29.227:6008";
+=======
+const String apiURl = "api.powermotoring.com";
+//const String apiURl = "api-new.powermotoring.com";
+//const String apiURl = "192.168.1.17:6008";
+>>>>>>> Stashed changes
 const String razorpayKey = "rzp_live_kIzyuqDpckalO0";
 // const String razorpayKey = "rzp_test_QZO2uhnxnX2rCX";
